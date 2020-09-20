a76f58d45ac1888502d2e9657afdbbb1afd6fcb0
n0remac <smartcameron@gmail.com>
2020-09-20 13:19:36 -0700

    Changed to simple physics engine.

--- Constants/Physics.py
+++ Constants/Physics.py
@@ -1,23 +1,2 @@
-# --- Physics forces. Higher number, faster accelerating.
-
-# Gravity
-GRAVITY = 1500
-
-# Damping - Amount of speed lost per second
-DEFAULT_DAMPING = .0001
-PLAYER_DAMPING = 0.4
-
-# Friction between objects
-PLAYER_FRICTION = 1.0
-WALL_FRICTION = 0.7
-DYNAMIC_ITEM_FRICTION = 0.6
-
-# Mass (defaults to 1)
-PLAYER_MASS = 2.0
-
-# Keep player from going too fast
-PLAYER_MAX_HORIZONTAL_SPEED = 450
-PLAYER_MAX_VERTICAL_SPEED = 1600
-
-# Force applied while on the ground
-PLAYER_MOVE_FORCE_ON_GROUND = 8000
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
--- Core/GameInstance.py
+++ Core/GameInstance.py
@@ -1,6 +1,6 @@
 import arcade
 
-from Constants.Physics import PLAYER_MOVE_FORCE_ON_GROUND
+from Constants.Physics import PLAYER_MOVEMENT_SPEED
 from Core.GameResources import GameResources
 from Physics.PhysicsEngine import setup_physics_engine
 
@@ -21,34 +21,29 @@ class GameInstance:
         # Set background color
         arcade.set_background_color(arcade.color.AMAZON)
 
-        # Track the current state of what key is pressed
-        # TODO: Figure out where we want to store state variables.
-        self.left_pressed: bool = False
-        self.right_pressed: bool = False
-        self.up_pressed: bool = False
-        self.down_pressed: bool = False
-
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
-        if key == arcade.key.LEFT:
-            self.left_pressed = True
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = True
-        elif key == arcade.key.UP:
-            self.up_pressed = True
-        elif key == arcade.key.DOWN:
-            self.down_pressed = True
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.game_resources.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.game_resources.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.game_resources.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.game_resources.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
-        if key == arcade.key.LEFT:
-            self.left_pressed = False
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = False
-        elif key == arcade.key.UP:
-            self.up_pressed = False
-        elif key == arcade.key.DOWN:
-            self.down_pressed = False
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.game_resources.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.game_resources.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.game_resources.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.game_resources.player_sprite.change_x = 0
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.game_resources.on_mouse_motion(x, y, dx, dy)
@@ -59,34 +54,8 @@ class GameInstance:
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Update player forces based on keys pressed
-        if self.left_pressed and not self.right_pressed:
-            # Create a force to the left. Apply it.
-            force = (-PLAYER_MOVE_FORCE_ON_GROUND, 0)
-            self.physics_engine.apply_force(self.game_resources.player_sprite, force)
-            # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.game_resources.player_sprite, 0)
-        elif self.right_pressed and not self.left_pressed:
-            # Create a force to the right. Apply it.
-            force = (PLAYER_MOVE_FORCE_ON_GROUND, 0)
-            self.physics_engine.apply_force(self.game_resources.player_sprite, force)
-            # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.game_resources.player_sprite, 0)
-        elif self.up_pressed and not self.down_pressed:
-            # Create a force to the right. Apply it.
-            force = (0, PLAYER_MOVE_FORCE_ON_GROUND)
-            self.physics_engine.apply_force(self.game_resources.player_sprite, force)
-            # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.game_resources.player_sprite, 0)
-        elif self.down_pressed and not self.up_pressed:
-            # Create a force to the right. Apply it.
-            force = (0, -PLAYER_MOVE_FORCE_ON_GROUND)
-            self.physics_engine.apply_force(self.game_resources.player_sprite, force)
-            # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.game_resources.player_sprite, 0)
-        else:
-            # Player's feet are not moving. Therefore up the friction so we stop.
-            self.physics_engine.set_friction(self.game_resources.player_sprite, 1.0)
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
-        self.physics_engine.step()
-        self.game_resources.on_update(delta_time)
+        # self.physics_engine.step()
+        # self.game_resources.on_update(delta_time)
