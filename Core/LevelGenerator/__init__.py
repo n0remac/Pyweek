a0b06941b745433bb98ef9c989fafa217c1434b8
Free Wortley <00free@gmail.com>
2020-09-21 12:34:04 -0700

    Add license notice

---
+++ Core/LevelGenerator/__init__.py
@@ -0,0 +1,3 @@
+# Code in this folder is forked/inspired from:
+# https://github.com/AtTheMatinee/dungeon-generation
+# Licensed under MIT: https://github.com/AtTheMatinee/dungeon-generation/blob/master/LICENSE
